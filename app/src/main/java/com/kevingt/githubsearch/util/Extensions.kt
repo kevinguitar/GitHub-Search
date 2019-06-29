10c4a5ec22cfe7e930d6bc007c538a0a738cf5d7
kevin-2017pro <kevin85619@gmail.com>
2019-06-29 10:49:30 +0800

    chore: create util package and ApiManager
    add Constants and Kotlin Extensions class
    ApiManager: A singleton class to allow ViewModel call apis

---
+++ app/src/main/java/com/kevingt/githubsearch/util/Extensions.kt
@@ -0,0 +1,21 @@
+package com.kevingt.githubsearch.util
+
+import com.kevingt.githubsearch.model.HttpResult
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+
+/**
+ * @return  HttpResult: convert from Response, to be more easy to handle
+ */
+suspend fun <T> Deferred<Response<T>>.convertToHttpResult(): HttpResult<Response<T>> {
+    return try {
+        val result = await()
+        if (result.isSuccessful) {
+            HttpResult.Success(result)
+        } else {
+            HttpResult.ApiError(result.message())
+        }
+    } catch (e: Throwable) {
+        HttpResult.NetworkError(e)
+    }
+}
